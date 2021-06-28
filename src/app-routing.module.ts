1f877ae6b801d234d08e85f75b2f5912f0a33677
Yoshi <63078117+YoshiiTsuu@users.noreply.github.com>
2021-06-28 11:36:58 -0300

    adicionando menu e rodape

---
+++ src/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingRoutingModule } from './app/app-routing/app-routing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppRoutingRoutingModule
+  ]
+})
+export class AppRoutingModule { }
