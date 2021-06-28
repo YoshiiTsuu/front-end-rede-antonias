be780a67b4e5296895897ea2214dbfbcbf2e6f52
Yoshi <63078117+YoshiiTsuu@users.noreply.github.com>
2021-06-28 12:50:26 -0300

    adicionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { MenuQuemSomosComponent } from './menu-quem-somos/menu-quem-somos.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    MenuQuemSomosComponent,
+    QuemSomosComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu-quem-somos/menu-quem-somos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-quem-somos',
+  templateUrl: './menu-quem-somos.component.html',
+  styleUrls: ['./menu-quem-somos.component.css']
+})
+export class MenuQuemSomosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
