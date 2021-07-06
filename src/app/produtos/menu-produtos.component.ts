a1f633cd4f079c721e5974b2610d1f8a46c6125e
Natália <na.ssilva@outlook.com>
2021-07-06 17:13:11 -0300

    atualizando crud

--- src/app/produtos/menu-produtos.component.ts
+++ src/app/produtos/menu-produtos.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutosServicos } from '../model/ProdutosServicos';
+import { UsuarioService } from '../service/usuario.service';
 
 @Component({
   selector: 'app-menu-produtos',
   templateUrl: './menu-produtos.component.html',
   styleUrls: ['./menu-produtos.component.css']
-})
+})  
 export class MenuProdutosComponent implements OnInit {
 
-  constructor() { }
+  listaProduto: ProdutosServicos[]
+  constructor(
+    private produtoService: UsuarioService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.findAllProdutos()
   }
 
+  findAllProdutos() {
+    this.produtoService.getAllProdutos().subscribe((resp: ProdutosServicos[]) => {
+      this.listaProduto = resp
+    })
+  }
 }
